fc840c40e2230236f5edd22d7b89f33824f1fda4
Evil Rabbit <hello@evilrabb.it>
2019-02-23 04:50:17 -0300

    Structure header, intro and projects layout

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,69 @@
 import Link from "next/link";
 import Header from "../components/header";
+import Image from "../components/image";
 
 export default () => (
   <main>
     <Header />
-    <section>
-      <Link href="/about">
-        <a>Go to About Me</a>
-      </Link>
+    <section className="intro">
+      <h1>Hi, I'm <span className="line-through"><b>Nicolás Garro</b>, a.k.a</span> <b>Evil Rabbit.</b></h1>
+      <h3>Head of Design at <b>ZEIT</b></h3>
     </section>
+    <section className="zeit">
+      <h4>ZEIT</h4>
+      <h2><b>Website</b></h2>
+      <Image
+        src={`https://res.cloudinary.com/evilrabbit/image/upload/v1550905163/www/works/zeit/website.png`}
+        width={2800/2.25}
+        height={2102/2.25}
+        margin={0}
+      />
+    </section>
+    <style global jsx>{`
+      ::selection {
+        background: #000;
+        color: #FFF;
+      }
+      body {
+        font-family: "SF Pro Text","SF Pro Icons","Helvetica Neue","Helvetica","Arial",sans-serif;
+        margin: 0;
+      }
+      b {
+        font-weight: 600;
+      }
+      h1 {
+        font-size: 6.250em;
+        font-weight: 300;
+        margin: 0;
+      }
+      h2 {
+        font-size: 3.375em;
+        margin: 0;
+      }
+      h3 {
+        font-size: 1.625em;
+        font-weight: 300;
+      }
+      h4 {
+        margin: 0;
+      }
+      .intro {
+        max-width: 980px;
+      }
+      .line-through {
+        text-decoration:line-through;
+      }
+      section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        height: 100vh;
+        margin: auto;
+      }
+      .zeit {
+        text-align: center;
+        max-width: 980px;
+      }
+    `}</style>
   </main>
 );
